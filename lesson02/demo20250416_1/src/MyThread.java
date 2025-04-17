04e951d93bd34d8e67f4d0c2811a40d29ad96b13
OleinikovaIrina <irenolgaruda@gmail.com>
2025-04-17 19:44:11 +0200

    3 lesson

--- lesson02/demo20250416_1/src/MyThread.java
+++ lesson02/demo20250416_1/src/MyThread.java
@@ -2,8 +2,6 @@ public class MyThread extends Thread{
 
     @Override
     public void run() {
-        String name = Thread.currentThread().getName();
-        System.out.println("Thread " + name + " start");
 
         for (int i = 0; i < 10; i++) {
             System.out.println( name + " " + i);
--- lesson03/demo20251704_1/src/Main.java
+++ lesson03/demo20251704_1/src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Thread task2 = new Task(2);
+        Thread task3 = new Task(3);
 
-        
+        task3.setDaemon(true);
+        task2.setDaemon(true);
+
+        task2.start();
+        task3.start();
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
