5dbefda1d0deb5efe1682b8f23b74ad0890fb541
OleinikovaIrina <irenolgaruda@gmail.com>
2025-05-17 18:28:45 +0200

    Add lesson18

---
+++ lesson18/userApi05_16/userApi/src/main/java/de/ait/userapi/dto/CategoryRequestDto.java
@@ -0,0 +1,17 @@
+package de.ait.userapi.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+
+public class CategoryRequestDto {
+
+private String name;
+}
---
+++ lesson18/userApi05_16/userApi/src/main/java/de/ait/userapi/model/Address.java
@@ -0,0 +1,21 @@
+package de.ait.userapi.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Embeddable
+public class Address {
+
+    private String country;
+    private String city;
+
+
+}
